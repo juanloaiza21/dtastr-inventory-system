7a5706e03b174ea2d9b56ad5bb82623716d86e1b
jpastor1649 <129129208+jpastor1649@users.noreply.github.com>
2023-04-04 16:03:14 -0500

    Add files via upload
    left add users

---
+++ src/Item.java
@@ -0,0 +1,86 @@
+package Selling;
+
+import java.util.*;
+import java.sql.*;
+
+public class Item {
+    private int id;
+
+    private String name;
+    private double price;
+    private int stock;
+
+    public Item(int id, String name, double price, int stock) {
+        this.name = name;
+        this.id = id;
+        this.price = price;
+        this.stock = stock;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    //get an item from the linked list
+    public static Item getItem(LinkedList<Item> list, String name) {
+        for (Item item : list) {
+            if (item.getName().equals(name)) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    // get items from the database
+    public static LinkedList<Item> getItems(Connection connection) throws SQLException {
+        String query = "SELECT * FROM PRODUCTS";
+        Statement statement = connection.createStatement();
+        ResultSet result = statement.executeQuery(query);
+
+        LinkedList<Item> itemsList = new LinkedList<>();
+        while (result.next()) { // result.next() = true while there are more items,
+
+            // to create an object "Item" with the data and add it to the list
+            int id = result.getInt("id"); // "id" or 1
+            String name = result.getString("name"); // "name" or 2
+            double price = result.getDouble("price"); // "price " or 3
+            int stock = result.getInt("stock"); // "stock" or 4
+
+            itemsList.add(new Item(id, name, price, stock));
+
+        }
+        result.close();
+
+        statement.close();
+        return itemsList;
+    }
+
+}
---
+++ src/SellItems.java
@@ -0,0 +1,65 @@
+package Selling;
+
+import java.sql.*;
+import java.util.*;
+
+public class SellItems {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Connect to DataBase
+        String url = "jdbc:mysql://localhost:3306/DTAPROYECT";
+        String user = "root";
+        String password = "alejo2425";
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            LinkedList<Item> itemsList = Item.getItems(connection); // insert items into a linkedlist
+
+            System.out.println("Which item do u wanna buy: ");
+            String name = scan.next().toLowerCase();
+            System.out.println("How many: ");
+            int Amount = scan.nextInt();
+
+            Item itemToSell = Item.getItem(itemsList, name);
+            if (itemToSell != null) {
+                if (itemToSell.getStock() >= Amount) {
+                    itemToSell.setStock(itemToSell.getStock() - Amount);
+                    System.out.println("Product selled: " + itemToSell.getName());
+                    System.out.println("Thanks for your purchase");
+
+                    updateStock(connection, itemToSell);
+                } else {
+                    System.out.println("There are not enough stock! ");
+                }
+            } else {
+                System.out.println("Item not found!");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally { // this closes the connection to the database
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        scan.close();
+
+    }
+
+    public static void updateStock(Connection connection, Item itemToSell) throws SQLException {
+        // statement for sql to update
+        String updateStock = "UPDATE PRODUCTS SET stock =" + itemToSell.getStock() + " WHERE id =" + itemToSell.getId();
+        Statement updStatement = connection.createStatement();
+        updStatement.executeUpdate(updateStock); // execute the statement
+
+        updStatement.close();
+
+    }
+
+}
