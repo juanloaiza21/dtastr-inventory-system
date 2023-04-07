a184613d4d093141f7a326b437b273f06f0ee99a
Juan David Loaiza Reyes <joumanmaster_@outlook.es>
2023-04-07 11:12:22 -0500

    Try catch on user module

--- src/users/Main.java
+++ src/users/Main.java
@@ -19,45 +19,53 @@ public class Main {
      * Create an user
      */
     public void createUser(){
-        Scanner sc = new Scanner(System.in);
-        String[] data = new String[6];
-        System.out.println("Enter the id of the user ");
         try {
-            data[0] = Integer.toString(sc.nextInt());
+            Scanner sc = new Scanner(System.in);
+            String[] data = new String[6];
+            System.out.println("Enter the id of the user ");
+            try {
+                data[0] = Integer.toString(sc.nextInt());
+            } catch (Exception e) {
+                System.err.println("The id must be a number");
+            }
+            System.out.println("Enter the name of the user ");
+            sc.nextLine();
+            data[1] = sc.nextLine();
+            System.out.println("Enter the email of the user ");
+            data[2] = sc.nextLine();
+            System.out.println("Enter the cellphone of the user ");
+            data[3] = sc.nextLine();
+            System.out.println("Enter 1 if the user its seller, enter 2 if its an normal user");
+            int aux = sc.nextInt();
+            if (aux==1) data[4] = "SELLER";
+            else if (aux==2) data[4] = "USER";
+            else {
+                System.out.println("The role must be 'SELLER' or 'BUYER'");
+            }
+            sc.nextLine();
+            System.out.println("Enter the password of the user ");
+            data[5] = sc.nextLine();
+            user.createUser(data);
         } catch (Exception e) {
-            System.err.println("The id must be a number");
+            System.err.println(e.getMessage());
         }
-        System.out.println("Enter the name of the user ");
-        sc.nextLine();
-        data[1] = sc.nextLine();
-        System.out.println("Enter the email of the user ");
-        data[2] = sc.nextLine();
-        System.out.println("Enter the cellphone of the user ");
-        data[3] = sc.nextLine();
-        System.out.println("Enter 1 if the user its seller, enter 2 if its an normal user");
-        int aux = sc.nextInt();
-        if (aux==1) data[4] = "SELLER";
-        else if (aux==2) data[4] = "USER";
-        else {
-            System.out.println("The role must be 'SELLER' or 'BUYER'");
-        }
-        sc.nextLine();
-        System.out.println("Enter the password of the user ");
-        data[5] = sc.nextLine();
-        user.createUser(data);
     }
 
     /**
      * Get an user
      */
     public void getUser(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the id of the user ");
-        int id = sc.nextInt();
         try {
-           user.getOneUser(id);
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the id of the user ");
+            int id = sc.nextInt();
+            try {
+               user.getOneUser(id);
+            } catch (Exception e) {
+                System.err.println("The id must be a number");
+            }
         } catch (Exception e) {
-            System.err.println("The id must be a number");
+            System.err.println(e.getMessage());
         }
     }
 
@@ -66,19 +74,18 @@ public class Main {
      * @implNote Create an implementation if its necesary
      */
     private void updateUser() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the id of the user ");
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     /**
      * Update the password of an user
      */
     public void updatePass(){
+        try {
         String[] data = new String[2];
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the email of the user ");
         String email = sc.nextLine();
-        try {
             data[0] = user.getByEmail(email);
             System.out.println("Enter the new password ");
             data[1] = sc.nextLine();
@@ -93,12 +100,12 @@ public class Main {
      * Bool that represents the login an user
     */
     public Boolean login() {
+        try {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the email of the user ");
         String email = sc.nextLine();
         System.out.println("Enter the password of the user ");
         String password = sc.nextLine();
-        try {
             return user.login(email, password);
         } catch (Exception e) {
             System.err.println("The email or password are incorrect");
@@ -112,9 +119,13 @@ public class Main {
      * @throws SQLException
      */
     public void deleteUser() throws SQLException {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the id of the user ");
-        user.deleteUser(sc.nextInt());
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the id of the user ");
+            user.deleteUser(sc.nextInt());
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 
     /**
