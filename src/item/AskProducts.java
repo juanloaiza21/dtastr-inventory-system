daecdb34e9b19b2f88faf9487ebb564ee83bca78
jpastor1649 <129129208+jpastor1649@users.noreply.github.com>
2023-05-05 10:04:59 -0500

    Add files via upload

--- src/item/AskProducts.java
+++ src/item/AskProducts.java
@@ -6,6 +6,7 @@ import db.Conector;
 
 /**
  * Class AskProducts
+ * 
  * @author john pastor
  *
  */
@@ -14,35 +15,35 @@ public class AskProducts {
     private Conector conector;
 
     public AskProducts() {
-        //conector = new Conector("jdbc:mysql://localhost:3306/DTAPROYECT", "root", "alejo2425");
-        conector = new Conector("jdbc:mysql://localhost:3306/DTAPROYECT", "root", "PCTdkx58");
-        conector.setTable("PRODUCTS");
+        conector = new Conector("jdbc:mysql://localhost:3306/DTAPROYECT", "root", "alejo2425");
+        // conector = new Conector("jdbc:mysql://localhost:3306/DTAPROYECT", "root",
+        // "PCTdkx58");
+        conector.setTable("USERS");
     }
-    
+
     public void Asking() throws SQLException {
         Scanner scan = new Scanner(System.in);
         Queue<Item> data = new LinkedList<>();
         Boolean controller = true;
         while (controller) {
             try {
-                    System.out.println("do u wanna ask a product: 1. yes, 2. no");
-                    int flag = scan.nextInt();
-                    if (flag == 2) {
-                        controller = false;
-                    } else if(flag == 1){
-                        System.out.println("name:");
-                        String name = scan.next();
-                        scan.nextLine();
-                        System.out.println("how much stock: ");
-                        int stock = scan.nextInt();
-                        data.add(new Item(0, name, 0, stock));
-                        adding(data);
-                        scan.nextLine();
-                        controller = false;
-                    }
-                    else {
-                        System.out.println("Incorrect Option");
-                    }
+                System.out.println("do u wanna ask a product: 1. yes, 2. no");
+                int flag = scan.nextInt();
+                if (flag == 2) {
+                    controller = false;
+                } else if (flag == 1) {
+                    System.out.println("name:");
+                    String name = scan.next();
+                    scan.nextLine();
+                    System.out.println("how much stock: ");
+                    int stock = scan.nextInt();
+                    data.add(new Item(0, name, 0, stock));
+                    adding(data);
+                    scan.nextLine();
+                    controller = false;
+                } else {
+                    System.out.println("Incorrect Option");
+                }
             } catch (InputMismatchException e) {
                 // TODO: handle exception
                 System.err.println("Must be int");
@@ -50,6 +51,7 @@ public class AskProducts {
             }
         }
 
+        scan.close();
     }
 
     private void adding(Queue<Item> data) throws SQLException {
@@ -63,6 +65,38 @@ public class AskProducts {
         }
         data.clear();
 
+    }
+
+    private LinkedList<ItemA> chargeData() {
+        LinkedList<ItemA> items = new LinkedList<>();
+        try {
+            conector.connect();
+            ResultSet result = conector.getAllResultSet();
+            while (result.next()) {
+                int id = result.getInt("id");
+                String name = result.getString("name");
+                double price = result.getDouble("price");
+                int stock = result.getInt("stock");
+                items.add(new Item(id, name, price, stock));
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return items;
+
+    }
+
+    public LinkedList<ItemA> getItems() throws SQLException {
+        LinkedList<ItemA> items = new LinkedList<>();
+        items = chargeData();
+        return items;
+    }
+
+    public static void main(String[] args) throws SQLException {
+        AskProducts a = new AskProducts();
+        // a.Asking();
+        LinkedList<ItemA> list = a.getItems();
+        System.out.println(list.get(0).name);
     }
 
 }
